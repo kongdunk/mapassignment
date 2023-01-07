61b6710d4f84636d7e35a9f2bb69fae4830b71df
daesanTheRiceologist <dkim265@my.bcit.ca>
2023-01-07 09:07:52 -0800

    changed to milenium line

--- components/Map/map.js
+++ components/Map/map.js
@@ -6,178 +6,131 @@ import style from '../../styles/Home.module.css'
 import { MapContainer, TileLayer, Marker, Popup, Polyline, CircleMarker } from 'react-leaflet'
 
 export default function Map() {
-    const position = [49.286312103271484, -123.11234283447266]; // waterfront
 
-    const blueOption = { color: 'blue' };
-    const pinkOption = { color: '#FF0090' };
+    const yellowOption = { color: 'yellow' };
     const redOption = { color: 'red' };
 
     const polyline = [
-        [49.286312103271484, -123.11234283447266], // waterfront
-        [49.2856364, -123.1198152], // burrard
-        [49.283302307128906, -123.11593627929688], //grandville
-        [49.279537200927734, -123.10968017578125], //stadium-chinatown
-        [49.27312088012695, -123.100341796875], // main street-science world
-        [49.26260757446289, -123.06910705566406], //commercial-broadway
-        [49.248374, -123.0560202], // nanaimo
-        [49.24420166015625, -123.0459213256836], //29th avenue
-        [49.2384033203125, -123.03174591064453], //joyce-collingwood
-        [49.2298294, -123.012788], //patterson
-        [49.2258524, -123.0038944], //metrotown
-        [49.2200742, -122.988416], //royal oak
-        [49.2121286, -122.959233], //edmonds
-        [49.200096130371094, -122.94883728027344], //22nd street
-        [49.201446533203125, -122.9125747680664], //new westminster
-        [49.205135345458984, -122.90621948242188], //columbia
-        [49.20444869995117, -122.8740234375], //scott road
-        [49.1991139, -122.850605], //gateway
-        [49.18951416015625, -122.84786224365234], //surrey central
-        [49.18256759643555, -122.84412384033203], //king george
+        [49.264929, -123.079008], // VCC Clark
+        [49.262864, -123.069841], // Commercial-Broadway
+        [49.258737, -123.045442], // RenFrew
+        [49.260313, -123.033716], // Rupert
+        [49.264735, -123.014321], // Gilmore
+        [49.266217, -123.001338], // Brentwood-Town Centre
+        [49.264890, -122.982295], // Holdom
+        [49.259669, -122.964235], // Sperling-Burnaby Lake
+        [49.254495, -122.938975], // Lake City Way
+        [49.253400, -122.918700], // Production Way University
+        [49.248326, -122.897229], // Lougheed Town Centre
+        [49.261096, -122.889601], // Burquitlam
+        [49.277835, -122.846699], // Moody Centre
+        [49.277077, -122.827795], // Inlet Centre
+        [49.274507, -122.800309], // Coqutialm Central
+        [49.280123, -122.794049], // Lincoln
+        [49.285636, -122.791962], // Lafarge Lake
     ];
 
-    const polyline2 = [
-        [49.205135345458984, -122.90621948242188], //columbia
-        [49.224860238697396, -122.88949270294468], //sapperton
-        [49.23268213068468, -122.88456287225554], //braid
-        [49.24875892482227, -122.89717730665], //lougheed town centre
-        [49.25367827613695, -122.91868207173422], //production way-university
-    ]
-
     return (
-        <MapContainer className={style.map} center={position} zoom={11} scrollWheelZoom={true}>
+        <MapContainer className={style.map} center={polyline[7]} zoom={11} scrollWheelZoom={true}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={position}>
+            <Marker position={polyline[0]}>
                 <Popup>
-                    Waterfront
+                    VCC Clark
                 </Popup>
             </Marker>
             <Marker position={polyline[1]}>
                 <Popup>
-                    Burrard
+                    Commercial-Broadway
                 </Popup>
             </Marker>
             <Marker position={polyline[2]}>
                 <Popup>
-                    Grandville
+                    RenFrew
                 </Popup>
             </Marker>
             <Marker position={polyline[3]}>
                 <Popup>
-                    Stadium-Chinatown
+                    Rupert
                 </Popup>
             </Marker>
             <Marker position={polyline[4]}>
                 <Popup>
-                    Main Street-Science World
+                    Gilmore
                 </Popup>
             </Marker>
             <Marker position={polyline[5]}>
                 <Popup>
-                    Commercial-Broadway
+                    Brentwood-TOwn Centre
                 </Popup>
             </Marker>
             <Marker position={polyline[6]}>
                 <Popup>
-                    Nanaimo
+                    Holdom
                 </Popup>
             </Marker>
             <Marker position={polyline[7]}>
                 <Popup>
-                    29th Avenue
+                    Sperling-Burnaby Lake
                 </Popup>
             </Marker>
             <Marker position={polyline[8]}>
                 <Popup>
-                    Joyce-Collingwood
+                    Lake City Way
                 </Popup>
+
             </Marker>
             <Marker position={polyline[9]}>
                 <Popup>
-                    Patterson
+                    Production Way University
                 </Popup>
-
             </Marker>
             <Marker position={polyline[10]}>
-                <Popup>
-
-                    Metrotown
-                </Popup>
-            </Marker>
-            <Marker position={polyline[11]}>
-                <Popup>
-                    Royal Oak
-                </Popup>
-            </Marker>
-            <Marker position={polyline[12]}>
-                <Popup>
-                    Edmonds
-                </Popup>
-            </Marker>
-            <Marker position={polyline[13]}>
-                <Popup>
-                    22nd Street
-                </Popup>
-            </Marker>
-            <Marker position={polyline[14]}>
-                <Popup>
-                    New Westminster
-                </Popup>
-            </Marker>
-            <Marker position={polyline[15]}>
-                <Popup>
-                    Columbia
-                </Popup>
-            </Marker>
-            <Marker position={polyline[16]}>
-                <Popup>
-                    Scott Road
-                </Popup>
-            </Marker>
-            <Marker position={polyline[17]}>
-                <Popup>
-                    Gateway
-                </Popup>
-            </Marker>
-            <Marker position={polyline[18]}>
-                <Popup>
-                    Surrey Central
-                </Popup>
-            </Marker>
-            <Marker position={polyline[19]}>
-                <Popup>
-                    King George
-                </Popup>
-            </Marker>
-
-            <Marker position={polyline2[1]}>
-                <Popup>
-                    Sapperton
-                </Popup>
-            </Marker>
-            <Marker position={polyline2[2]}>
-                <Popup>
-                    Braid
-                </Popup>
-            </Marker>
-            <Marker position={polyline2[3]}>
                 <Popup>
                     Lougheed Town Centre
                 </Popup>
             </Marker>
-            <Marker position={polyline2[4]}>
+            <Marker position={polyline[11]}>
                 <Popup>
-                    Production Way-University
+                    Burquitlam
                 </Popup>
             </Marker>
+            <Marker position={polyline[12]}>
+                <Popup>
+                    Moody Centre
+                </Popup>
+            </Marker>
+            <Marker position={polyline[13]}>
+                <Popup>
+                    Inlet Centre
+                </Popup>
+            </Marker>
+            <Marker position={polyline[14]}>
+                <Popup>
+                    Coquitlam Central
+                </Popup>
+            </Marker>
+            <Marker position={polyline[15]}>
+                <Popup>
+                    Lincoln 
+                </Popup>
+            </Marker>
+            
+            <Marker position={polyline[16]}>
+                <Popup>
+                    Lafarge Lake Douglas
+                </Popup>
+            </Marker>
+            
+
+
+
 
             <CircleMarker center={polyline[0]} pathOptions={redOption} radius={10}></CircleMarker>
-            <CircleMarker center={polyline[19]} pathOptions={redOption} radius={10}></CircleMarker>
-            <CircleMarker center={polyline2[4]} pathOptions={redOption} radius={10}></CircleMarker>
-            <Polyline pathOptions={blueOption} positions={polyline} />
-            <Polyline pathOptions={pinkOption} positions={polyline2} />
+            <CircleMarker center={polyline[16]} pathOptions={redOption} radius={10}></CircleMarker>
+            <Polyline pathOptions={yellowOption} positions={polyline} />
         </MapContainer>
     )
 
